4d4fd1e4ba5b279e9cca4d612ba7e0a41da46449
Louis Tiao <louistiao@gmail.com>
2015-04-10 16:16:25 +1000

    new notebook

--- project.cpp
+++ project.cpp
@@ -1,17 +1,42 @@
-#include "opencv2/opencv.hpp"
+#include <iostream>
 
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/features2d/features2d.hpp"
+// #include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+
+
+using namespace std;
 using namespace cv;
 
-int main(int, char**)
-{
-    VideoCapture cap("inputs/clip_test.m4v");
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
+int main(int argc, char** argv) {
+
+	if(argc < 3) {
+		cerr << "Usage: " << argv[0] << " <video> <patch>" << endl;
+		return -1;
+	}
+
+	string video_fname = argv[1];
+	string patch_fname = argv[2];
+
+	VideoCapture cap(video_fname);
+    if (!cap.isOpened()) {
+    	cerr << "Unable to capture video " << video_fname << endl;
+    	return -1;
+    }
+
+    Mat patch = imread(patch_fname);
+
+    SiftFeatureDetector detector;
+	
+	vector<KeyPoint> keypoints_1;
+	
+	detector.detect(patch, keypoints_1);
 
     Mat edges;
-    namedWindow("edges",1);
-    for(;;)
-    {
+    namedWindow("Image Matching", WINDOW_AUTOSIZE);
+    while(true) {
         Mat frame;
         cap >> frame; // get a new frame from camera
         cvtColor(frame, edges, CV_BGR2GRAY);
@@ -20,6 +45,7 @@ int main(int, char**)
         imshow("edges", edges);
         if(waitKey(30) >= 0) break;
     }
+
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
