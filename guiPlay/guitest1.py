98e20465bb34ff05bc3acf7a537c669d401ed283
Edward Lee <edward.lee.1@live.com>
2015-04-11 16:19:44 +1000

    fixes

--- guiPlay/guitest1.py
+++ guiPlay/guitest1.py
@@ -9,44 +9,36 @@ def mouseCallback(event,x,y,flags,param):
     global sx, sy, ex, ey, drawing, frame
     if event == cv2.EVENT_LBUTTONDOWN:
         sx, sy, ex, ey, drawing = x, y, x, y, True
-        print "button done: {}, {}".format(sx, sy)
     if event == cv2.EVENT_LBUTTONUP:
         drawing = False
         cv2.namedWindow('Crop')
         if (sx != x and sy != y):
             if (sx > x): sx, x = x, sx
             if (sy > y): sy, y = y, sy
-            print "buttonup: " + repr(sx) +"," + repr(sy) + " - " + repr(x) + "," + repr(y)
-            # cv2.imshow('Crop', frame[sx:x, sy:y])
+            cv2.imshow('Crop', frame[sy:y, sx:x])
     if event == cv2.EVENT_MOUSEMOVE:
         if drawing:
-            print "mouse move: {}, {}".format(x,y)
             ex, ey = x,y
 
 def drawImage(img):
+    global sx, sy, ex, ey, drawing
     if drawing:
         img2 = np.copy(img)
+        cv2.rectangle(img2, (sx,sy), (ex,ey), (255,0,0), 2)
         cv2.imshow('Movie', img2)
     else:   
         cv2.imshow('Movie', img);
 
-
-# Create a black image, a window and bind the function to window
-# img = np.zeros((512,512,3), np.uint8)
-# cv2.namedWindow('image')
-
 drawing = False
-cap = cv2.VideoCapture('./clip_test.m4v')
+cap = cv2.VideoCapture('../inputs/clip_test.m4v')
 
 cv2.namedWindow('Movie')
 cv2.setMouseCallback('Movie', mouseCallback)
 
-
-
 ret = 1
 while ret:
     ret, frame = cap.read()
-    cv2.imshow('Movie', frame);
+    drawImage(frame)
     if (cv2.waitKey(30) >= 0): break;
 cap.release()
 
