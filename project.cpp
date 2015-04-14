87bce80a6897e4e09c9de06f5d83e623195c3f48
Louis Tiao <louistiao@gmail.com>
2015-04-14 15:45:23 +1000

    prototyping videowriter

--- project.cpp
+++ project.cpp
@@ -25,7 +25,16 @@ int main(int argc, char** argv) {
     	cerr << "Unable to capture video " << video_fname << endl;
     	return -1;
     }
+    cout << cap.get(CV_CAP_PROP_FPS) << endl;
 
+    // VideoWriter writer("out.m4v", cap.get(CV_CAP_PROP_FOURCC), cap.get(CV_CAP_PROP_FPS), Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH), (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT)));
+    VideoWriter writer("out.m4v", -1, cap.get(CV_CAP_PROP_FPS), Size((int) cap.get(CV_CAP_PROP_FRAME_WIDTH), (int) cap.get(CV_CAP_PROP_FRAME_HEIGHT)));
+
+    if (!writer.isOpened())
+    {
+        cerr << "Unable to open output video " << video_fname << endl;
+        return -1;
+    }
     Mat patch = imread(patch_fname);
 
     SiftFeatureDetector detector;
@@ -34,16 +43,20 @@ int main(int argc, char** argv) {
 	
 	detector.detect(patch, keypoints_1);
 
+
     Mat edges;
     namedWindow("Image Matching", WINDOW_AUTOSIZE);
     while(true) {
+
         Mat frame;
         cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
+        if (frame.empty()) break;
+        // cvtColor(frame, edges, CV_BGR2GRAY);
+        // GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        // Canny(edges, edges, 0, 30, 3);
+        // imshow("edges", frame);
+        writer << frame;
+        // if(waitKey(30) >= 0) break;
     }
 
     // the camera will be deinitialized automatically in VideoCapture destructor
